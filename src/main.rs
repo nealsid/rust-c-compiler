95b1691452a0c74f763bc06ba731a0c09cc6854d
Neal <nealsid@users.noreply.github.com>
2025-05-21 12:58:47 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = Path::new(&args[1]);
+
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("Read {} successfully", args[1]),
+    }
+}
