077858c26e85db67dfd78f4b7eec5dcffe8813bc
Neal <nealsid@users.noreply.github.com>
2025-05-22 16:16:32 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use regex::Regex;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+enum Token {
+    LeftParen,
+    RightParen,
+    Keyword,
+    LeftBrace,
+}
+
+struct TokenRegex {
+    regex : Regex,
+    token : Token,
+}
+
+
+fn tokenize() {
+    let token_regexes : Vec<TokenRegex> = vec![
+        TokenRegex { regex: Regex::new("^(").unwrap(), token: Token::LeftParen},
+        TokenRegex { regex: Regex::new("^)").unwrap(), token: Token::RightParen }];
+    
+
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = Path::new(&args[1]);
@@ -19,4 +40,6 @@ fn main() {
         Err(why) => panic!("couldn't read {}: {}", display, why),
         Ok(_) => print!("Read {} successfully", args[1]),
     }
+
+    
 }
