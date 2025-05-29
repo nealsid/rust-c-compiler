94cdcfd840b9c5e2f449eb1b455878e996a8577f
Neal <nealsid@users.noreply.github.com>
2025-05-29 13:59:18 -0400

    Fix data types of line number and column number fields, and move some code around.
    Line/column number fields should be usize, and move the Tokenizer
    creation method to the bottom of the file

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
 
 macro_rules! regex {
-    ($str:literal) => { Regex::new($str).unwrap() };
+    ($str:literal) => {
+        Regex::new($str).unwrap()
+    };
 }
 
 struct RegExAndToken {
@@ -11,9 +13,10 @@ struct RegExAndToken {
 
 #[derive(Debug)]
 pub struct TokenInfo {
-    token : Token,
-    line_number : u32,
-    column_number : u32
+    token: Token,
+    line_number: usize,
+    column_start: usize,
+    column_end: usize,
 }
 
 #[derive(Debug, Clone)]
@@ -37,6 +40,77 @@ pub struct Tokenizer {
 }
 
 impl Tokenizer {
+    pub fn tokenize(&self, buf: &str) -> Vec<TokenInfo> {
+        let mut slice_start: usize = 0;
+        let mut current_line_number: usize = 1;
+        let mut current_column_number: usize = 0;
+        let mut token_infos: Vec<TokenInfo> = Vec::new();
+        let mut matched_newline = false;
+
+        'outer_loop: while slice_start < buf.len() {
+            matched_newline = false;
+            // if match whitespace, advance slice_start
+            if let Some(whitespace_match) = regex![r"^[ \t]+"].find(&buf[slice_start..]) {
+                slice_start += whitespace_match.end();
+                current_column_number += whitespace_match.end();
+            }
+            // while match newline, increment line number and continue
+            while let Some(newline_match) = regex![r"^(\r?\n)|^(\n\r?)"].find(&buf[slice_start..]) {
+                slice_start += newline_match.end();
+                current_line_number += 1;
+                current_column_number = 0;
+                matched_newline = true;
+            }
+
+            // If we matched a newline, start over to consume whitespace again
+            if matched_newline {
+                continue;
+            }
+
+            for reg_ex_and_token in &self.reg_ex_and_tokens {
+                let next_token = reg_ex_and_token.regex.find(&buf[slice_start..]);
+                match next_token {
+                    Some(hit) => {
+                        // The hit returned by regex::find is relative to the slice
+                        // we passed into it, so we need to turn it into positions
+                        // that are relative to the beginning of buf.
+                        let match_start = slice_start + hit.start();
+                        let match_end = slice_start + hit.end();
+                        println!(
+                            "Matched {}: \"{}\"",
+                            reg_ex_and_token.regex,
+                            &buf[match_start..match_end]
+                        );
+                        println!("{} - {}", match_start, match_end);
+                        println!("{}", slice_start);
+                        let t: Token;
+                        t = match reg_ex_and_token.token {
+                            Token::Keyword { keyword: _ } => Token::Keyword {
+                                keyword: String::from(&buf[match_start..match_end]),
+                            },
+                            _ => reg_ex_and_token.token.clone(),
+                        };
+                        let token_info = TokenInfo {
+                            token: t,
+                            line_number: current_line_number,
+                            column_start: current_column_number,
+                            column_end: current_column_number + hit.end()
+                        };
+                        token_infos.push(token_info);
+                        slice_start += hit.end();
+                        current_column_number += hit.end();
+                        continue 'outer_loop;
+                    }
+                    None => {
+                        continue;
+                    }
+                }
+            }
+            panic!("Invalid token: \"{}\"", buf.as_bytes()[slice_start] as char);
+        }
+        token_infos
+    }
+
     pub fn new() -> Self {
         Tokenizer {
             reg_ex_and_tokens: vec![
@@ -88,71 +162,5 @@ impl Tokenizer {
                 },
             ],
         }
-    }
-
-    pub fn tokenize(&self, buf: &str) -> Vec<TokenInfo> {
-        let mut slice_start: usize = 0;
-        let mut current_line_number: u32 = 1;
-        let mut v: Vec<TokenInfo> = Vec::new();
-        let mut matched_newline = false;
-
-        'outer_loop: while slice_start < buf.len() {
-            matched_newline = false;
-            // if match whitespace, advance slice_start
-            if let Some(whitespace_match) = regex![r"^[ \t]+"]
-                .find(&buf[slice_start..])
-            {
-                slice_start += whitespace_match.end();
-            }
-            // while match newline, increment line number and continue
-            while let Some(newline_match) = regex![r"^(\r?\n)|^(\n\r?)"]
-                .find(&buf[slice_start..])
-            {
-                slice_start += newline_match.end();
-                current_line_number += 1;
-                matched_newline = true;
-            }
-
-            // If we matched a newline, start over to consume whitespace again
-            if matched_newline {
-                continue;
-            }
-
-            for reg_ex_and_token in &self.reg_ex_and_tokens {
-                let next_token = reg_ex_and_token.regex.find(&buf[slice_start..]);
-                match next_token {
-                    Some(hit) => {
-                        // The hit returned by regex::find is relative to the slice
-                        // we passed into it, so we need to turn it into positions
-                        // that are relative to the beginning of buf.
-                        let match_start = slice_start + hit.start();
-                        let match_end = slice_start + hit.end();
-                        println!("Matched {}: \"{}\"", reg_ex_and_token.regex, &buf[match_start..match_end]);
-                        println!("{} - {}", match_start, match_end);
-                        println!("{}", slice_start);
-                        let t: Token;
-                        t = match reg_ex_and_token.token {
-                            Token::Keyword { keyword: _ } => Token::Keyword {
-                                keyword: String::from(&buf[match_start..match_end]),
-                            },
-                            _ => reg_ex_and_token.token.clone(),
-                        };
-                        let token_info = TokenInfo {
-                            token: t,
-                            line_number: current_line_number,
-                            column_number: 0
-                        };
-                        v.push(token_info);
-                        slice_start += hit.end();
-                        continue 'outer_loop;
-                    }
-                    None => {
-                        continue;
-                    }
-                }
-            }
-            panic!("Invalid token: \"{}\"", buf.as_bytes()[slice_start] as char);
-        }
-        v
     }
 }
